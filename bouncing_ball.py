95cd5b58b6ab06373936966e7ad19adb8c9a9a6d
Yewon Lee <yewonlee@Yewons-MacBook-Pro.local>
2023-12-21 09:31:59 +0900

    add xpbd sim for pendulum and bouncing ball

---
+++ bouncing_ball.py
@@ -0,0 +1,176 @@
+import numpy as np
+import math
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
+from matplotlib.colors import Normalize
+from matplotlib.cm import ScalarMappable
+
+class BouncingBall:
+    '''
+    2D bouncing ball-in-a-box example:
+
+    There are N number of balls which have some initial starting position and velocity
+    The balls are enclosed in a bounded box and can move around anywhere inside this box
+    '''
+
+    def __init__(self):
+
+        # Ball-in-a-box with gravity: simulation setup
+        self.radii = np.array([0.2 , 0.4])
+        self.masses = (self.radii ** 2) * 100
+        self.n_balls = self.radii.shape[0]
+        self.p = np.array([[0.5, 2.5], 
+                           [2.5, 2.5]])
+        self.p_prev = np.zeros((2, self.n_balls))
+        self.v = np.array([[30, -30],
+                           [30, 30]])
+        self.g = 0 #-9.81
+        self.skin_thickness = 0.02 # artificial skin thickness to allow for earlier collision detection
+
+        # Number of constraints per ball
+        self.n_constraints = 5 # XXX: considering contact with walls and between 2 balls
+
+        # Box configuration
+        self.w = 3
+        self.h = 3
+
+        # XPBD parameter setup
+        self.xpbd_iterations = 1000
+        self.T = 5
+        self.sim_dt = 0.01
+        self.alpha = math.pow(10, -5) / math.pow(self.sim_dt, 2)
+
+        # Set up Matplotlib figure and axis
+        self.fig, self.ax = plt.subplots()
+        self.ax.set_xlim(0, self.w)
+        self.ax.set_ylim(0, self.h)
+        self.ax.set_title('Bouncing Balls in a Box', fontsize=14)
+
+        # Set aspect ratio to 'equal'
+        self.ax.set_aspect('equal') 
+
+        # Set the background color to black
+        self.ax.set_facecolor('black')
+
+        # Create colors for the balls
+        brighter_pastel_colors = ['#FFA1B5', '#FFE872', '#77FFEA', '#8AC9FF', '#FFA573']
+        self.colors = brighter_pastel_colors[:self.n_balls]
+        self.circles = [plt.Circle((self.p[0, i], self.p[1, i]), self.radii[i], fill=True, color=self.colors[i]) for i in range(self.n_balls)]
+        [self.ax.add_patch(circle) for circle in self.circles]
+
+        # Create quiver arrows for velocity representation
+        self.quiver_arrows = [self.ax.quiver(0, 0, 0, 0, color='black', scale_units='xy', scale=10) for i in range(self.n_balls)]
+
+
+    def compute_constraints(self, x, y):
+        # Inputs: 
+        #       x and y are (n_balls, 2) arrays
+        # Outputs:
+        #       returns constraints c, which are (n_balls, n_constraints) arrays 
+        #      
+
+        c = np.zeros((self.n_balls, self.n_constraints))
+        for i in range(self.n_balls):
+            c[i, 0] = x[i] - self.radii[i]
+            c[i, 1] = y[i] - self.radii[i]
+            c[i, 2] = self.w - (x[i] + self.radii[i])
+            c[i, 3] = self.h - (y[i] + self.radii[i])
+        c[0, 4] = (x[0] - x[1])** 2 + (y[0] - y[1])**2 - (self.radii[0] + self.radii[1] + self.skin_thickness)**2 # XXX: we assume 2 balls for the ball-ball collision constraint
+        c[1, 4] = (x[0] - x[1])** 2 + (y[0] - y[1])**2 - (self.radii[0] + self.radii[1] + self.skin_thickness)**2 # XXX: we assume 2 balls for the ball-ball collision constraint
+        return c
+    
+    def compute_grad_constraints(self, x, y):
+        # Inputs:
+        #       x: (n_balls, 2) arrays
+        #       y: (n_balls, 2) arrays
+        #       c: (n_balls, n_constraints) constraint arrays 
+        # Output: 
+        #       returns gradient of constraints wrt position
+        #       array of size (n_balls, n_constraints, 2)
+        
+        grad_c = np.zeros((self.n_balls, self.n_constraints, 2))
+        grad_c[:, 0, :] = np.array([1, 0])
+        grad_c[:, 1, :] = np.array([0, 1])
+        grad_c[:, 2, :] = np.array([-1, 0])
+        grad_c[:, 3, :] = np.array([0, -1])
+        grad_c[0, 4, :] = np.array([2*(x[0]-x[1]), 2*(y[0]-y[1])])
+        grad_c[1, 4, :] = np.array([2*(x[1]-x[0]), 2*(y[1]-y[0])])
+
+        return grad_c
+
+
+    def compute_lambda_step(self, c, lmda, grad_c, ball_id):
+        grad_c_norm = np.sqrt(grad_c[ball_id, 4, 0]**2 + grad_c[ball_id, 4, 1]**2)
+        denom = (np.sum(self.masses) * (self.n_constraints-1) + grad_c_norm * self.masses[ball_id] + self.alpha)
+        return (- c - self.alpha * lmda) / denom
+    
+
+    def compute_pos_step(self, lmda_step, grad_c, ball_id):
+        return lmda_step * grad_c * (1/self.masses[ball_id]) 
+
+
+    def xpbd_step(self):
+
+        # initialize solve & multiplier
+        vx = self.v[0,:] 
+        vy = self.v[1,:] + self.sim_dt * self.g
+        p_pred = self.p + self.sim_dt * np.vstack([vx, vy])
+        lmda_pred = np.zeros((self.n_balls, self.n_constraints)) # dimensions are same as total number of constraints
+
+        for i in range(self.xpbd_iterations):
+
+            # compute constraints
+            c = self.compute_constraints(p_pred[0, :], p_pred[1, :]) # array of size (n_balls, 4)
+            grad_c = self.compute_grad_constraints(p_pred[0, :], p_pred[1, :]) # array of size (n_balls, 4, 2)
+
+            for j in range(self.n_balls):
+                for k in range(self.n_constraints):
+                    if c[j, k] < 0:
+                        # update lambda
+                        lmda_step = self.compute_lambda_step(c[j, k], lmda_pred[j, k], grad_c, j)
+                        lmda_pred[j, k] += lmda_step
+
+                        # update position
+                        p_pred[:, j] += self.compute_pos_step(lmda_step, grad_c[j, k, :], j)
+
+        self.v = (p_pred - self.p) / self.sim_dt
+        self.p = p_pred
+
+
+    def render_arrows(self):
+
+        # Normalize the velocity vector
+        for i in range(self.n_balls):
+            v_norm = np.linalg.norm(self.v[:, i])
+            normalized_v = self.v[:, i] / v_norm if v_norm != 0 else np.zeros(2)
+
+            self.quiver_arrows[i].set_offsets(self.p[:, i])
+            self.quiver_arrows[i].set_UVC(normalized_v[0], normalized_v[1])
+
+    def render(self):
+
+        for i in range(self.n_balls):
+            self.circles[i].center = (self.p[0, i], self.p[1, i])
+
+        self.render_arrows()
+
+        # Pause for a short time to create a real-time effect
+        plt.pause(0.01)
+
+            
+    def simulate_step(self, frame):
+        print(self.p)
+        self.xpbd_step()
+        self.render()
+
+
+# Instantiate the bouncing ball class
+bounce = BouncingBall()
+
+# Set up the animation
+num_frames = int(bounce.T/bounce.sim_dt)
+ani = FuncAnimation(bounce.fig, bounce.simulate_step, frames=num_frames, interval=50, blit=False)
+
+
+# Display the animation
+plt.show()
--- pendulum.py
+++ pendulum.py
@@ -1,73 +1,160 @@
 import numpy as np
 import math
-from renderer import Render
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
 
 class Pendulum:
+    '''
+    2D pendulum simulation:
+
+    Pendulum with different masses has some initial starting condition (zero initial velocity, non zero position)
+    The effect of gravity acting on the pendulum is simulated
+    '''
 
     def __init__(self):
 
         # Pendulum simulation setup
-        masses = np.array([0.5, 0.5, 0.5])
-        n_masses = masses.shape[0]
-        lengths = np.array([1, 1, 1])
-        thetas = [math.pi/8, math.pi/6, math.pi/4]
-        p = {'x': np.array([0, 0, 0]), 
-             'y': np.array([0, 0, 0])}
-        p_prev = {'x': np.array([0, 0, 0]), 
-                  'y': np.array([0, 0, 0])}
-        v = {'x': np.array([0, 0, 0]), 
-             'y': np.array([0, 0, 0])}
-        g = -9.81
+        self.masses = np.array([1.0, 0.5, 0.3, 2.0])
+        self.n_masses = self.masses.shape[0]
+        self.lengths = np.array([1, 1, 1, 1])
+        thetas = [math.pi/8, math.pi/8, math.pi/8, math.pi/8]
+        self.p = np.zeros((2, self.n_masses))
+        self.p_prev = np.zeros((2, self.n_masses))
+        self.v = np.zeros((2, self.n_masses))
+        self.g = -9.81
 
         # Initialize mass positions
-        for i in range(n_masses):
-            if i==0:
+        for i in range(self.n_masses):
+            if i == 0:
                 prev_mass_p = {'x': 0, 'y': 0}
             else:
-                prev_mass_p = {'x': p['x'][i-1], 'y': p['y'][i-1]}
-            p['x'][i] = prev_mass_p['x'] + lengths[i] * math.cos(thetas[i])
-            p['y'][i] = prev_mass_p['x'] + lengths[i] * math.sin(thetas[i])
+                prev_mass_p = {'x': self.p[0, i - 1], 'y': self.p[1, i - 1]}
+            self.p[0, i] = prev_mass_p['x'] - self.lengths[i] * math.cos(thetas[i])
+            self.p[1, i] = prev_mass_p['y'] - self.lengths[i] * math.sin(thetas[i])
+
 
         # XPBD parameter setup
-        xpbd_iterations = 10
-        T = 5
-        sim_dt = 0.1
-        alpha = 0.5 * self.ones(self.n_masses) / (sim_dt^2)
+        self.xpbd_iterations = 1000
+        self.T = 5
+        self.sim_dt = 0.01
+        self.alpha = math.pow(10, -9) * np.ones(self.n_masses) / math.pow(self.sim_dt, 2)
+
+        # Set up Matplotlib figure and axis
+        self.fig, self.ax = plt.subplots()
+        self.ax.set_xlim(-6, 6)
+        self.ax.set_ylim(-6, 4)
+        self.ax.set_title('2D Pendulum', fontsize=14)
+
+        # Set aspect ratio to 'equal'
+        self.ax.set_aspect('equal') 
+
+        # Set the background color to black
+        self.ax.set_facecolor('black')
+
+        # Create lines representing the pendulum
+        self.lines = [self.ax.plot([], [], '-', lw=2)[0] for _ in range(self.n_masses)]
+        self.markers = [self.ax.scatter([], [], s=300 * np.sqrt(self.masses[i]), color='white', marker='o') for i in range(self.n_masses)]
 
 
     def compute_constraints(self, x, y):
         c = np.zeros(self.n_masses)
         for i in range(self.n_masses):
             if i==0:
-                c[i] = np.sqrt((x[i]-0)^2 + (y[i]-0)^2)
+                c[i] = (np.sqrt(math.pow(x[i]-0, 2) + math.pow(y[i]-0, 2)) - self.lengths[i])**2
             else:
-                c[i] = np.sqrt((x[i]-x[i-1])^2 + (y[i]-y[i-1])^2)
+                c[i] = (np.sqrt(math.pow(x[i]-x[i-1], 2) + math.pow(y[i]-y[i-1], 2)) - self.lengths[i])**2
         return c
     
-    def compute_grad_constraints(self):
-        return np.ones(self.n_masses)
+    def compute_grad_constraints(self, x, y):
+        # Inputs:
+        #       x: numpy array of size (num_masses)
+        #       y: numpy array of size (num_masses)
+        #       c: constrats; numpy array of size (num_masses)
+        # Output: returns gradient of constraint wrt position
+        #         array of size (2, num_constraints)
+        grad_c = np.zeros((2, self.n_masses))
+        for i in range(self.n_masses):
+            if i==0:
+                norm = np.sqrt(math.pow(x[i]-0, 2) + math.pow(y[i]-0, 2))
+                grad_c[0, i] = ((x[i]-0) / norm) * 2 * (norm - self.lengths[i])
+                grad_c[1, i] = ((y[i]-0) / norm) * 2 * (norm - self.lengths[i])
+            else:
+                norm = np.sqrt(math.pow(x[i]-x[i-1], 2) + math.pow(y[i]-y[i-1], 2))
+                grad_c[0, i] = ((x[i] - x[i-1]) / norm) * 2 * (norm - self.lengths[i])
+                grad_c[1, i] = ((y[i] - y[i-1]) / norm) * 2 * (norm - self.lengths[i])
+        return grad_c
     
     def compute_lambda_step(self, c, lmda):
+        # Inputs:
+        #   c: numpy of constraints evaluated with current position; dimension is # of constraints
+        #   lmda: numpy of lambdas; dimensions is # of constraints
+        # Outputs:
+        #   return lambda step, which is an array where shape is number of constraints
+
         return (- c - self.alpha * lmda) / (np.sum(self.masses) + self.alpha)
     
-    def compute_x_step(self, grad_c, lmda_step):
-        return lmda_step * 
+    def compute_pos_step(self, lmda_step, p_pred):
+        return lmda_step * (1/self.masses) * self.compute_grad_constraints(p_pred[0, :], p_pred[1, :])
 
 
     def xpbd_step(self):
 
         # initialize solve & multiplier
-        vx = self.v['x'] 
-        vy = self.v['y'] + dt * self.g
-        x_pred = self.p['x'] + dt * vx
-        y_pred = self.p['y'] + dt * vy
-        lambda_pred = np.zeros(self.n_masses)
-
-        dt = self.sim_dt / self.xpbd_iterations
+        vx = self.v[0,:] 
+        vy = self.v[1,:] + self.sim_dt * self.g
+        p_pred = self.p + self.sim_dt * np.vstack([vx, vy])
+        lmda_pred = np.zeros(self.n_masses) # dimensions are same as number of constraints
 
         for i in range(self.xpbd_iterations):
-            continue
+            # compute constraints
+            c = self.compute_constraints(p_pred[0, :], p_pred[1, :])
+            # update lambda
+            lmda_step = self.compute_lambda_step(c, lmda_pred)
+            lmda_pred += lmda_step
+            # update position
+            p_pred += self.compute_pos_step(lmda_step, p_pred)
 
-        # project constraints
-        
-            
+        self.v = (p_pred - self.p) / self.sim_dt
+        self.p = p_pred
+
+
+    def render(self):
+        # Update the positions and colors of the masses and the lines connecting them
+        for i in range(self.n_masses):
+            # Set the data for lines with a lower zorder
+            self.lines[i].set_data(self.p[0, i], self.p[1, i])
+            self.lines[i].set_zorder(1)  # Lower zorder for lines
+
+            # Set the data and color for markers with a higher zorder
+            self.markers[i].set_offsets([self.p[0, i], self.p[1, i]])
+            line_color = self.lines[i].get_color()
+            self.markers[i].set_facecolor(line_color)
+            self.markers[i].set_zorder(2)  # Higher zorder for markers
+
+        # Update the positions of the lines connecting the masses
+        for i in range(self.n_masses - 1):
+            line_x = [self.p[0, i], self.p[0, i + 1]]
+            line_y = [self.p[1, i], self.p[1, i + 1]]
+            self.lines[i + 1].set_data(line_x, line_y)
+
+        # Update the position of the line connecting the first mass to the origin
+        self.lines[0].set_data([0, self.p[0, 0]], [0, self.p[1, 0]])
+
+        # Pause for a short time to create a real-time effect
+        plt.pause(0.01)
+
+
+    def simulate_step(self, frame):
+        self.xpbd_step()
+        self.render()
+
+
+# Instantiate the Pendulum class
+pendulum = Pendulum()
+
+# Set up the animation
+num_frames = int(pendulum.T/pendulum.sim_dt)
+ani = FuncAnimation(pendulum.fig, pendulum.simulate_step, frames=num_frames, interval=50, blit=False)
+
+# Display the animation
+plt.show()
